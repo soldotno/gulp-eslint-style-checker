b4d03e92d085094d632d333479bf49ae1cd65063
anuva <andrew@uvarov.ru>
2014-12-17 16:15:19 +0300

    NO-ISSUE: fix checking style-check itself, allow '? :' operator to be split, restrict line to 120 chars

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var styleCheck = require('./lib/jscs/jscsChecker');
 
 // Run code style check
-gulp.task('style-check', styleCheck('lib/index.js', "lib/jscs/.jscsrc"));
+gulp.task('style-check', styleCheck('', 'lib/jscs/.jscsrc'));
 
 // Default Task
 gulp.task('default', ['develop']);
