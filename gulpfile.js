39f1677d02dca2fde7aa1450944b599bb03eaf68
Leonid Brish <leonid.v.brish@gmail.com>
2015-02-06 12:40:22 +0300

    HARV-162: Style checker should check jsx: esprima-fb added

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var styleCheck = require('./lib/jscs/jscsChecker');
 
 // Run code style check
-gulp.task('style-check', styleCheck('', 'lib/jscs/.jscsrc'));
+gulp.task('style-check', styleCheck('', 'lib/jscs/default.jscsrc'));
 
 // Default Task
 gulp.task('default', ['develop']);
