a50d95bb50c3c47b6e79f3d177c77e0586b25ee8
sisve <sinsvend@gmail.com>
2015-03-31 00:22:49 +0200

    Fixing dependencies problems
    added esprima-fb and jscs as peerDependencies. Was not able to get esprima-fb to work as a dependencies. There where also some problems with changes in esprima-fb that affected jscs.
    Both are upgraded to the latest version, that is a minimun to get the esprima-fb to work.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require('gulp');
 var styleCheck = require('./lib/jscs/jscsChecker');
 
-// Run code style check
-gulp.task('style-check', styleCheck('', 'lib/jscs/default.jscsrc'));
-
-// Default Task
-gulp.task('default', ['develop']);
+gulp.task('style-check', styleCheck('', 'esprima-fb'));
+gulp.task('default', ['style-check']);
