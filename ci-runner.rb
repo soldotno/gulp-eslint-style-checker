9b1282833ced15093b91d0a9c0f5c5d5669a5741
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-03-08 11:55:22 +0100

    Update ci-runner.rb

--- ci-runner.rb
+++ ci-runner.rb
@@ -1,8 +1,5 @@
 # Compares the files that has been changed between this branch and master.
 #
-# USAGE
-#       curl https://raw.githubusercontent.com/soldotno/gulp-eslint-style-checker/master/ci-runner.rb | ruby
-#
 # AUTHOR
 #       Martin Stabenfeldt <martin@stabenfeldt.net>
 #
@@ -14,7 +11,7 @@ GITHUB_REPO = open('.git/config').grep(/github/).first.match(/.*:(.*).git/)[1]
 def remove_missing_files(files)
   return if files.empty?
   existing_files = files.split.select { |file| File.exists?(file) }
-  existing_files.join(' ') if existing_files.size > 1
+  existing_files.join(' ')
 end
 
 def style_check_modfied_files
@@ -22,7 +19,7 @@ def style_check_modfied_files
   if ENV['CI']
     # The master branch is not available on the build server.
     token = ENV['GITHUB_TOKEN']
-    url   = "https://api.github.com/repos/#{GITHUB_REPO}/compare/master...#{current_sha}?access_token=#{token}"
+    url   = "https://api.github.com/repos/#{GITHUB_REPO}/compare/master...#{current_sha}?access_token = #{token}"
     files = `curl -i #{url} | grep filename | cut -f2 -d: | grep \.js | tr '"', '\ '`
   else
     files = `git diff master #{current_sha} --name-only | grep .js`
@@ -30,14 +27,15 @@ def style_check_modfied_files
   files.tr!("\n", ' ')
   cleaned = remove_missing_files(files)
 
-  if cleaned.size > 1
+  if cleaned.size >= 1
     puts "\n\nInspecting #{cleaned}"
     system("npm install gulp-eslint-style-checker") unless system("grep gulp-eslint-style-checker package.json")
+    puts "Running #{STYLE_CHECKER} #{cleaned}"
+    @report = `#{STYLE_CHECKER} #{cleaned}`
   else
     puts 'No changes made'
   end
 
-  @report = `#{STYLE_CHECKER} #{cleaned}` if cleaned.size > 1
 
   if @report && @report.size > 1
     puts @report
