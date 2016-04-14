2fc9a2173b7a03e19ef3810df204b7b38bd67375
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-04-14 09:34:16 +0200

    Specify which version of packages to be installed

--- ci-runner.rb
+++ ci-runner.rb
@@ -11,13 +11,33 @@ ESLINT        = './node_modules/eslint/bin/eslint.js'
 ESLINT_CONFIG = 'eslintrc.json-ci-runner'
 CONFIG_URL    =  'https://raw.githubusercontent.com/soldotno/gulp-eslint-style-checker/master/eslintrc.json'
 STYLE_CHECKER = "#{ESLINT} -c #{ESLINT_CONFIG}"
+GIT_CONFIG    = '.git/config'
 
-GIT_CONFIG = '.git/config'
 raise "We need #{GIT_CONFIG}" unless File.exist?(GIT_CONFIG)
 GITHUB_REPO   = open(GIT_CONFIG).grep(/github/).first.match(/.*:(.*).git/)[1]
 
-
 raise "You forgot to set ENV['GITHUB_TOKEN']" unless ENV['GITHUB_TOKEN']
+
+def style_check_modfied_files
+  files = modified_files
+  if files
+    download_config
+    puts 'Installing eslint..'
+    system('npm i babel babel-eslint eslint@2.7.0 eslint-plugin-jsx-a11y@latest eslint-plugin-react@4.3.0 eslint-config-airbnb gulp-eslint minimist --save-dev')
+    puts "\n\n Running #{STYLE_CHECKER} #{files} \n\n"
+    @report = `#{STYLE_CHECKER} #{files}`
+  else
+    puts 'No changes made'
+  end
+
+  if @report && @report.size > 1
+    puts @report
+    exit 1
+  else
+    puts "Well done! All modified_files are according to our style guide! :-) "
+  end
+end
+
 
 def remove_missing_files(files)
   return if files.empty?
@@ -43,33 +63,12 @@ def modified_files
     url   = "https://api.github.com/repos/#{GITHUB_REPO}/compare/master...#{current_sha}?access_token=#{ENV['GITHUB_TOKEN']}"
     files = `curl -i #{url} | grep filename | cut -f2 -d: | grep .js | grep -v .json | tr '"', '\ '`
   else
-    puts "not CI"
     files = `git diff master #{current_sha} --name-only | grep .js | grep -v .json`
   end
   files.tr!("\n", ' ')
   files.size >= 1 ? remove_missing_files(files) : false
 end
 
-
-def style_check_modfied_files
-  files = modified_files
-  if files
-    download_config
-    puts 'Installing eslint..'
-    system('npm i babel babel-eslint eslint@2.7.0 eslint-config-airbnb eslint-plugin-react gulp-eslint minimist --save-dev')
-    puts "\n\n Running #{STYLE_CHECKER} #{files} \n\n"
-    @report = `#{STYLE_CHECKER} #{files}`
-  else
-    puts 'No changes made'
-  end
-
-  if @report && @report.size > 1
-    puts @report
-    exit 1
-  else
-    puts "Well done! All modified_files are according to our style guide! :-) "
-  end
-end
 
 
 style_check_modfied_files
